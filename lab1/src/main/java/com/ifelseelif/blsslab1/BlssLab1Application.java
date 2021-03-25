9bdd52ced7e81ebe21701a542c7c927318c5e8e8
Artem Kolokolov <yougrysha@gmail.com>
2021-03-25 20:39:40 +0300

    Init

---
+++ lab1/src/main/java/com/ifelseelif/blsslab1/BlssLab1Application.java
@@ -0,0 +1,13 @@
+package com.ifelseelif.blsslab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlssLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlssLab1Application.class, args);
+    }
+
+}
---
+++ lab1/src/test/java/com/ifelseelif/blsslab1/BlssLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifelseelif.blsslab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlssLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
