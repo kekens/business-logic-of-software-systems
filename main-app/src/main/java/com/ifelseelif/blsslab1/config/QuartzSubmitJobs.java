056274861e8f9d8e925379f7232809a0c61d9835
kirill <kirillbobryakov82@gmail.com>
2021-06-08 23:46:24 +0300

    quartz init (doesn't work)

---
+++ main-app/src/main/java/com/ifelseelif/blsslab1/config/QuartzSubmitJobs.java
@@ -0,0 +1,15 @@
+package com.ifelseelif.blsslab1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+
+@Configuration
+public class QuartzSubmitJobs {
+
+//    @Bean(name = "memberStats")
+//    public JobDetailFactoryBean jobMemberStats() {
+//        return QuartzConfig.createJobDetail(MemberStatsJob.class, "Member Statistics Job");
+//    }
+
+}
